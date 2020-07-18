947204a3f34f2cf0fc8fd433fd37978712c63315
Moo Cow <rootuid@gmail.com>
2020-07-18 15:21:36 +0100

    initial commit

---
+++ Readme.md
@@ -0,0 +1,16 @@
+Tests for Dummy api
+
+Installation
+- Install requirements pip3 intall -r requirements.txt -U
+
+Execution
+- pytest test.py
+
+
+
+
+
+
+
+
+
---
+++ test.py
@@ -0,0 +1,14 @@
+import requests
+
+baseurl = "http://dummy.restapiexample.com/api/v1/"
+
+def func(x):
+        return x + 1
+
+def test_answer():
+    assert func(3) == 5
+
+def test_employee():
+    response = request.GET f({baseurl}{employee})
+    print (response)
+
